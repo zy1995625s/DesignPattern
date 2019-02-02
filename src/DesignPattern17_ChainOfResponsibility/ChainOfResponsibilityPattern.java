2c639cfa6ad994b05a7342ae96be9057d8e4ec6a
zy1995625s <zy1995625s@gmail.com>
2019-02-02 12:24:57 +0800

    添加 责任链 ChainOfResponsibility 模式

---
+++ src/DesignPattern17_ChainOfResponsibility/ChainOfResponsibilityPattern.java
@@ -0,0 +1,78 @@
+package DesignPattern17_ChainOfResponsibility;
+
+/**
+ * 为了避免请求发送者与多个请求处理者耦合在一起，将所有请求的处理者通过前一对象记住其下一个对象的引用而连成一条链；
+ * 当有请求发生时，可将请求沿着这条链传递，直到有对象处理它为止。
+ * 
+ * 职责链模式主要包含以下角色。
+ * 抽象处理者（Handler）角色：定义一个处理请求的接口，包含抽象处理方法和一个后继连接。
+ * 具体处理者（Concrete Handler）角色：实现抽象处理者的处理方法，判断能否处理本次请求，如果可以处理请求则处理，否则将该请求转给它的后继者。
+ * 客户类（Client）角色：创建处理链，并向链头的具体处理者对象提交请求，它不关心处理细节和请求的传递过程。
+
+ * @author YidaZhang
+ *
+ */
+
+public class ChainOfResponsibilityPattern {
+
+	public static void main(String[] args) {
+        //组装责任链 
+        Handler handler1=new ConcreteHandler1(); 
+        Handler handler2=new ConcreteHandler2(); 
+        handler1.setNext(handler2); 
+        //提交请求 
+        handler1.handleRequest("two");
+	}
+	
+}
+
+//抽象处理者角色
+abstract class Handler {
+	private Handler next;
+	
+	public void setNext(Handler next) {
+		this.next = next;
+	}
+	
+	public Handler getNext() {
+		return next;
+	}
+	
+    //处理请求的方法
+    public abstract void handleRequest(String request);
+}
+
+//具体处理者角色1
+class ConcreteHandler1 extends Handler {
+    public void handleRequest(String request) {
+        if(request.equals("one")) {
+            System.out.println("具体处理者1负责处理该请求！");       
+        }
+        else {
+            if(getNext()!=null) {
+                getNext().handleRequest(request);             
+            }
+            else {
+                System.out.println("没有人处理该请求！");
+            }
+        } 
+    }
+}
+
+//具体处理者角色2
+class ConcreteHandler2 extends Handler {
+	public void handleRequest(String request) {
+        if(request.equals("two")) {
+            System.out.println("具体处理者2负责处理该请求！");       
+        }
+        else {
+            if(getNext()!=null) {
+                getNext().handleRequest(request);             
+            }
+            else {
+                System.out.println("没有人处理该请求！");
+            }
+        } 
+    }
+}
+
